121395c24cfa8bc8a42076232c36845d00c49abe
Restyled.io <commits@restyled.io>
2024-01-19 10:38:01 +0000

    Restyled by prettier

--- react/src/component/BaseView.jsx
+++ react/src/component/BaseView.jsx
@@ -3,11 +3,11 @@
 
 import React from "react";
 import axios from "@/axios";
-import {withStyles} from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
-import {createTheme} from "@mui/material/styles";
-import { initialize } from 'react-native-clarity';
+import { createTheme } from "@mui/material/styles";
+import { initialize } from "react-native-clarity";
 
 initialize("kj0ror1pki");
 
@@ -16,152 +16,175 @@ initialize("kj0ror1pki");
  * https://material-ui.com/customization/palette/
  */
 export const StyledTableCell = withStyles((theme) => ({
-    head: {
-        backgroundColor: theme.palette.primary.dark,
-        color: theme.palette.common.white,
-    },
-    body: {
-        fontSize: 14,
-    },
+  head: {
+    backgroundColor: theme.palette.primary.dark,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
 }))(TableCell);
 
 export const StyledTableRow = withStyles((theme) => ({
-    root: {
-        '&:nth-of-type(odd)': {
-            backgroundColor: theme.palette.action.selected,
-        },
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.selected,
     },
+  },
 }))(TableRow);
 
 export const darkTheme = createTheme({
-    palette: {
-        mode: 'dark',
-    },
+  palette: {
+    mode: "dark",
+  },
 });
 
 export default class BaseView extends React.Component {
-    state = {
-        snackbarIsShown: false,
-        snackbarSeverity: null,
-        snackbarMessage: null,
+  state = {
+    snackbarIsShown: false,
+    snackbarSeverity: null,
+    snackbarMessage: null,
 
-        userInfo: null,
-        teamList: null,
+    userInfo: null,
+    teamList: null,
 
-        selectedTeamId: null,
-        selectedTeamName: null,
+    selectedTeamId: null,
+    selectedTeamName: null,
+  };
+
+  snackBarFail = (res) => {
+    let message = res + "";
+    if (res.data && res.data.message) {
+      message = res.data.message;
+    } else if (res.response && res.response.data && res.response.data.message) {
+      message = res.response.data.message;
     }
+    this.setState({
+      snackbarIsShown: true,
+      snackbarSeverity: "error",
+      snackbarMessage: message + "",
+    });
+  };
 
-    snackBarFail = (res) => {
-        let message = res + ""
-        if (res.data && res.data.message) {
-            message = res.data.message
-        } else if (res.response && res.response.data && res.response.data.message) {
-            message = res.response.data.message
+  snackBarError = (error) => {
+    console.log(error);
+    this.setState({
+      snackbarSeverity: "error",
+      snackbarMessage: error + "",
+      snackbarIsShown: true,
+    });
+  };
+
+  snackBarMsg = (msg) => {
+    console.log(msg);
+    this.setState({
+      snackbarIsShown: true,
+      snackbarSeverity: "error",
+      snackbarMessage: msg,
+    });
+  };
+
+  handleStatus = (target, state) => {
+    if (target !== null) {
+      this.setState({
+        [target]: state,
+      });
+      console.log(target + " => " + JSON.stringify(state));
+    }
+  };
+
+  handleValueChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+    console.log([event.target.name] + " => " + event.target.value);
+  };
+
+  handleValueSwitched = (event) => {
+    this.setState({
+      [event.target.name]: event.target.checked,
+    });
+    console.log([event.target.name] + " => " + event.target.checked);
+  };
+
+  handleFileUpload = (event) => {
+    this.setState({
+      [event.target.id]: event.target.files[0],
+    });
+    console.log(event.target.files[0].name);
+  };
+
+  getUserInfo = () => {
+    this.axiosGet("/api/auth/getUser", (content) => {
+      this.setState({
+        userInfo: content,
+        selectedTeamId: content.defaultTeamId,
+        selectedTeamName: content.defaultTeamName,
+      });
+    });
+  };
+
+  refreshTeamList() {
+    this.axiosGet("/api/userTeam/listSelfTeam", (content) => {
+      this.setState({ teamList: content });
+    });
+  }
+
+  axiosGet(url, handleResponse) {
+    axios
+      .get(url)
+      .then((res) => {
+        if (res.data && res.data.code === 200) {
+          handleResponse(res.data.content);
+        } else {
+          this.snackBarFail(res);
         }
-        this.setState({
-            snackbarIsShown: true,
-            snackbarSeverity: "error",
-            snackbarMessage: message + "",
+      })
+      .catch((error) => {
+        this.snackBarError(error);
+      });
+  }
+
+  axiosPost(url, handleResponse, postBody, formParams, formData, headers) {
+    if (postBody) {
+      axios
+        .post(url, postBody)
+        .then((res) => {
+          if (res.data && res.data.code === 200) {
+            handleResponse(res.data.content);
+          } else {
+            this.snackBarFail(res);
+          }
         })
-    }
-
-    snackBarError = (error) => {
-        console.log(error)
-        this.setState({
-            snackbarSeverity: "error",
-            snackbarMessage: error + "",
-            snackbarIsShown: true,
-        })
-    }
-
-    snackBarMsg = (msg) => {
-        console.log(msg)
-        this.setState({
-            snackbarIsShown: true,
-            snackbarSeverity: "error",
-            snackbarMessage: msg
-        })
-    }
-
-    handleStatus = (target, state) => {
-        if (target !== null) {
-            this.setState({
-                [target]: state
-            })
-            console.log(target + " => " + JSON.stringify(state))
-        }
-    }
-
-    handleValueChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
+        .catch((error) => {
+          this.snackBarError(error);
         });
-        console.log([event.target.name] + " => " + event.target.value)
-    }
-    
-    handleValueSwitched = (event) => {
-        this.setState({
-            [event.target.name]: event.target.checked
-        });
-        console.log([event.target.name] + " => " + event.target.checked)
-    }
-
-    handleFileUpload = (event) => {
-        this.setState({
-            [event.target.id]: event.target.files[0]
+    } else if (formParams && headers) {
+      axios
+        .post(url, formParams, { headers: headers })
+        .then((res) => {
+          if (res.data && res.data.code === 200) {
+            handleResponse(res.data.content);
+          } else {
+            this.snackBarFail(res);
+          }
         })
-        console.log(event.target.files[0].name)
+        .catch((error) => {
+          this.snackBarError(error);
+        });
+    } else if (formData && headers) {
+      axios
+        .post(url, formData, { headers: headers })
+        .then((res) => {
+          if (res.data && res.data.code === 200) {
+            handleResponse(res.data.content);
+          } else {
+            this.snackBarFail(res);
+          }
+        })
+        .catch((error) => {
+          this.snackBarError(error);
+        });
     }
-
-    getUserInfo = () => {
-        this.axiosGet('/api/auth/getUser', (content) => { this.setState({
-            userInfo: content,
-            selectedTeamId: content.defaultTeamId,
-            selectedTeamName: content.defaultTeamName,
-        }) })
-
-    }
-
-    refreshTeamList() {
-        this.axiosGet('/api/userTeam/listSelfTeam', (content) => { this.setState({ teamList: content }) })
-    }
-
-    axiosGet(url, handleResponse) {
-        axios.get(url).then(res => {
-            if (res.data && res.data.code === 200) {
-                handleResponse(res.data.content)
-            } else {
-                this.snackBarFail(res)
-            }
-        }).catch((error) => { this.snackBarError(error) })
-    }
-
-    axiosPost(url, handleResponse, postBody, formParams, formData, headers) {
-        if (postBody) {
-            axios.post(url, postBody).then(res => {
-                if (res.data && res.data.code === 200) {
-                    handleResponse(res.data.content)
-                } else {
-                    this.snackBarFail(res)
-                }
-            }).catch((error) => { this.snackBarError(error) })
-        } else if (formParams && headers) {
-            axios.post(url, formParams, { headers: headers }).then(res => {
-                if (res.data && res.data.code === 200) {
-                    handleResponse(res.data.content)
-                } else {
-                    this.snackBarFail(res)
-                }
-            }).catch((error) => { this.snackBarError(error) })
-        } else if (formData && headers) {
-            axios.post(url, formData, { headers: headers }).then(res => {
-                if (res.data && res.data.code === 200) {
-                    handleResponse(res.data.content)
-                } else {
-                    this.snackBarFail(res)
-                }
-            }).catch((error) => { this.snackBarError(error) })
-        }
-    }
+  }
 }
--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,112 +1,116 @@
-const path = require('path')
-const userConfig = require('./webpackUserConfig.json');
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require("copy-webpack-plugin")
-const TerserPlugin = require("terser-webpack-plugin")
-const webpack = require('webpack')
-const CompressionPlugin = require("compression-webpack-plugin")
+const path = require("path");
+const userConfig = require("./webpackUserConfig.json");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const webpack = require("webpack");
+const CompressionPlugin = require("compression-webpack-plugin");
 
 const htmlPlugin = new HtmlWebPackPlugin({
-  template : path.join(__dirname, './src/index.html'),
-  filename : 'index.html'
-})
-const distPath = '../center/src/main/resources/static/dist'
+  template: path.join(__dirname, "./src/index.html"),
+  filename: "index.html",
+});
+const distPath = "../center/src/main/resources/static/dist";
 // Change the 'webpack.config.json' to point it to your Hydra Lab API service
-const devHydraLabServer = userConfig.devServer
+const devHydraLabServer = userConfig.devServer;
 
 // About Terser https://webpack.js.org/plugins/terser-webpack-plugin/
 // about ENV usage: https://webpack.js.org/guides/environment-variables/
-module.exports = env => {
-  console.log("env is ", env)
+module.exports = (env) => {
+  console.log("env is ", env);
   return {
-    mode: env.prod ? "production" : "development", output: {
+    mode: env.prod ? "production" : "development",
+    output: {
       path: path.join(__dirname, distPath),
-      filename: 'bundle.js',
-      hashFunction: 'xxhash64'
+      filename: "bundle.js",
+      hashFunction: "xxhash64",
     },
-        devServer: {
-          port: 9999,
-          proxy: {
-            '/api': devHydraLabServer,
-            '/devices': devHydraLabServer,
-            '/test': devHydraLabServer,
-            '/images': {
-              target: devHydraLabServer,
-              pathRewrite: {'^/images': '/src/images'}
-            }
-          }
+    devServer: {
+      port: 9999,
+      proxy: {
+        "/api": devHydraLabServer,
+        "/devices": devHydraLabServer,
+        "/test": devHydraLabServer,
+        "/images": {
+          target: devHydraLabServer,
+          pathRewrite: { "^/images": "/src/images" },
         },
-        optimization: {
-          minimize: env.prod,
-          minimizer: [ new TerserPlugin() ],
+      },
+    },
+    optimization: {
+      minimize: env.prod,
+      minimizer: [new TerserPlugin()],
+    },
+    plugins: [
+      htmlPlugin,
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.join(__dirname, "images"),
+            to: path.join(__dirname, distPath + "/images"),
+          },
+        ],
+      }),
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^\.\/locale$/,
+        contextRegExp: /moment$/,
+      }),
+      new CompressionPlugin({ test: /\.js/, algorithm: "gzip" }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js|jsx$/,
+          use: "babel-loader",
+          exclude: /node_modules/,
         },
-        plugins:
-            [
-              htmlPlugin, new CopyPlugin({
-                patterns : [
-                  {
-                    from : path.join(__dirname, 'images'),
-                    to : path.join(__dirname, distPath + '/images')
-                  },
-                ],
-              }),
-              new webpack.IgnorePlugin(
-                  {resourceRegExp : /^\.\/locale$/, contextRegExp : /moment$/}),
-              new CompressionPlugin({test : /\.js/, algorithm : 'gzip'})
-            ],
-        module: {
-          rules:
-              [
-                {
-                  test : /\.js|jsx$/,
-                  use : 'babel-loader',
-                  exclude : /node_modules/
-                },
-                {
-                  test : /\.js$/,
-                  enforce : 'pre',
-                  use : [ 'source-map-loader' ],
-                  exclude : /node_modules/,
-                },
-                {
-                  test : /\.css$/,
-                  use : [ {loader : 'style-loader'}, {loader : 'css-loader'} ]
-                },
-                {
-                  test : /\.scss$/,
-                  exclude : /node_modules/,
-                  use : [
-                    {loader : 'style-loader'}, {
-                      loader : 'css-loader',
-                      options : {
-                        modules :
-                            {localIdentName : '[path]-[name]-[local]-[hash:6]'}
-                      }
-                    },
-                    {loader : 'sass-loader'}
-                  ]
-                },
-                {
-                  test : /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-                  use : [ {
-                    loader : "url-loader",
-                    options : {
-                      name : "[name].[hash:16].[ext]",
-                      limit : 1024 * 8,
-                      outputPath : "images",
-                      publicPath : "assets/imgs",
-                    },
-                  } ]
-                },
-              ]
+        {
+          test: /\.js$/,
+          enforce: "pre",
+          use: ["source-map-loader"],
+          exclude: /node_modules/,
         },
-        resolve: {
-          extensions: [ '.js', '.jsx', '.json' ],
-          alias: {
-            '@': path.join(__dirname, '/src'),
-            'react-native': 'react-native-web',
-          }
+        {
+          test: /\.css$/,
+          use: [{ loader: "style-loader" }, { loader: "css-loader" }],
         },
-        externals: {moment: 'moment'}
-  }
-}
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          use: [
+            { loader: "style-loader" },
+            {
+              loader: "css-loader",
+              options: {
+                modules: { localIdentName: "[path]-[name]-[local]-[hash:6]" },
+              },
+            },
+            { loader: "sass-loader" },
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+          use: [
+            {
+              loader: "url-loader",
+              options: {
+                name: "[name].[hash:16].[ext]",
+                limit: 1024 * 8,
+                outputPath: "images",
+                publicPath: "assets/imgs",
+              },
+            },
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx", ".json"],
+      alias: {
+        "@": path.join(__dirname, "/src"),
+        "react-native": "react-native-web",
+      },
+    },
+    externals: { moment: "moment" },
+  };
+};
