3bbdf217b4542dabc824808c347af43546b71bf4
Restyled.io <commits@restyled.io>
2024-01-19 10:37:44 +0000

    Restyled by clang-format

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -7,8 +7,8 @@ const webpack = require('webpack')
 const CompressionPlugin = require("compression-webpack-plugin")
 
 const htmlPlugin = new HtmlWebPackPlugin({
-    template: path.join(__dirname, './src/index.html'),
-    filename: 'index.html'
+  template : path.join(__dirname, './src/index.html'),
+  filename : 'index.html'
 })
 const distPath = '../center/src/main/resources/static/dist'
 // Change the 'webpack.config.json' to point it to your Hydra Lab API service
@@ -17,101 +17,96 @@ const devHydraLabServer = userConfig.devServer
 // About Terser https://webpack.js.org/plugins/terser-webpack-plugin/
 // about ENV usage: https://webpack.js.org/guides/environment-variables/
 module.exports = env => {
-    console.log("env is ", env)
-    return {
-        mode: env.prod?"production":"development",
-        output: {
-            path: path.join(__dirname, distPath),
-            filename: 'bundle.js',
-            hashFunction: 'xxhash64'
-        },
+  console.log("env is ", env)
+  return {
+    mode: env.prod ? "production" : "development", output: {
+      path: path.join(__dirname, distPath),
+      filename: 'bundle.js',
+      hashFunction: 'xxhash64'
+    },
         devServer: {
-            port: 9999,
-            proxy: {
-                '/api': devHydraLabServer,
-                '/devices': devHydraLabServer,
-                '/test': devHydraLabServer,
-                '/images': {
-                    target: devHydraLabServer,
-                    pathRewrite: { '^/images': '/src/images' }
-                }
+          port: 9999,
+          proxy: {
+            '/api': devHydraLabServer,
+            '/devices': devHydraLabServer,
+            '/test': devHydraLabServer,
+            '/images': {
+              target: devHydraLabServer,
+              pathRewrite: {'^/images': '/src/images'}
             }
+          }
         },
         optimization: {
-            minimize: env.prod,
-            minimizer: [new TerserPlugin()],
+          minimize: env.prod,
+          minimizer: [ new TerserPlugin() ],
         },
-        plugins: [
-            htmlPlugin,
-            new CopyPlugin({
-                patterns: [
-                    { from: path.join(__dirname, 'images'), to: path.join(__dirname, distPath + '/images') },
+        plugins:
+            [
+              htmlPlugin, new CopyPlugin({
+                patterns : [
+                  {
+                    from : path.join(__dirname, 'images'),
+                    to : path.join(__dirname, distPath + '/images')
+                  },
                 ],
-            }),
-            new webpack.IgnorePlugin({
-                resourceRegExp: /^\.\/locale$/,
-                contextRegExp: /moment$/
-            }),
-            new CompressionPlugin({
-                test: /\.js/,
-                algorithm: 'gzip'
-            })
-        ],
+              }),
+              new webpack.IgnorePlugin(
+                  {resourceRegExp : /^\.\/locale$/, contextRegExp : /moment$/}),
+              new CompressionPlugin({test : /\.js/, algorithm : 'gzip'})
+            ],
         module: {
-            rules: [
-                { test: /\.js|jsx$/, use: 'babel-loader', exclude: /node_modules/ },
+          rules:
+              [
                 {
-                    test: /\.js$/,
-                    enforce: 'pre',
-                    use: ['source-map-loader'],
-                    exclude: /node_modules/,
+                  test : /\.js|jsx$/,
+                  use : 'babel-loader',
+                  exclude : /node_modules/
                 },
                 {
-                    test: /\.css$/,
-                    use: [
-                        { loader: 'style-loader' },
-                        { loader: 'css-loader' }
-                    ]
+                  test : /\.js$/,
+                  enforce : 'pre',
+                  use : [ 'source-map-loader' ],
+                  exclude : /node_modules/,
                 },
                 {
-                    test: /\.scss$/,
-                    exclude: /node_modules/,
-                    use: [
-                        { loader: 'style-loader' },
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                modules: { localIdentName: '[path]-[name]-[local]-[hash:6]' }
-                            }
-                        },
-                        { loader: 'sass-loader' }
-                    ]
+                  test : /\.css$/,
+                  use : [ {loader : 'style-loader'}, {loader : 'css-loader'} ]
                 },
                 {
-                    test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-                    use: [
-                        {
-                            loader: "url-loader",
-                            options: {
-                                name: "[name].[hash:16].[ext]",
-                                limit: 1024 * 8,
-                                outputPath: "images",
-                                publicPath: "assets/imgs",
-                            },
-                        }
-                    ]
+                  test : /\.scss$/,
+                  exclude : /node_modules/,
+                  use : [
+                    {loader : 'style-loader'}, {
+                      loader : 'css-loader',
+                      options : {
+                        modules :
+                            {localIdentName : '[path]-[name]-[local]-[hash:6]'}
+                      }
+                    },
+                    {loader : 'sass-loader'}
+                  ]
                 },
-            ]
+                {
+                  test : /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                  use : [ {
+                    loader : "url-loader",
+                    options : {
+                      name : "[name].[hash:16].[ext]",
+                      limit : 1024 * 8,
+                      outputPath : "images",
+                      publicPath : "assets/imgs",
+                    },
+                  } ]
+                },
+              ]
         },
         resolve: {
-            extensions: ['.js', '.jsx', '.json'],
-            alias: {
-                '@': path.join(__dirname, '/src'),
-                'react-native': 'react-native-web',
-            }
+          extensions: [ '.js', '.jsx', '.json' ],
+          alias: {
+            '@': path.join(__dirname, '/src'),
+            'react-native': 'react-native-web',
+          }
         },
-        externals: {
-            moment: 'moment'
-        }
-    }
+        externals: {moment: 'moment'}
+  }
 }
